75213e65dc296121dd934c2597830a33ef7eca3d
volodymyr.vaskovych <wolodjawaskovych@i.ua>
2020-04-09 08:19:48 +0200

    Two components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { QuestionComponent } from './question/question.component';
+import { RequestsComponent } from './requests/requests.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionComponent,
+    RequestsComponent
   ],
   imports: [
     BrowserModule
