81e4a04f05716b90dadf320c51ac7b1b7dd67caf
volodymyr.vaskovych <wolodjawaskovych@i.ua>
2020-04-09 11:45:54 +0200

    Bootstrap, forms for elevators added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { QuestionComponent } from './question/question.component';
 import { RequestsComponent } from './requests/requests.component';
 import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +17,9 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-question',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
+
+  numberOfElevators: number;
+
+  showError : boolean = false;
 
   ngOnInit(): void {
+  }
+
+  validateAndProceed(): void {
+    if (this.numberOfElevators && this.numberOfElevators >= 0) {
+      this.router.navigate(['/requests/'+ this.numberOfElevators]);
+    } else{
+      this.showError = true;
+      console.log("Incorrect number of elevators!");
+    }
   }
 
 }
